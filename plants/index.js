e29079b6e82b072c1fae0e86f3cb626e56cb26b7
Andrei23232323-crypto <felbb4@gmail.com>
2023-01-22 23:50:45 +0100

    feat: add responsive for site

--- plants/index.js
+++ plants/index.js
@@ -1,7 +1,27 @@
-console.log('1. Вёрстка валидная +10\n' +
-    '2. Вёрстка семантическая +20\n' +
-    '3. Вёрстка соответствует макету +48\n' +
-    '4. Требования к css + 12\n' +
-    '5. Интерактивность, реализуемая через css +20\n' +
-    'Итого 110 баллов (все пункты выполнены)'
-);
+console.log('1. Вёрстка соответствует макету. Ширина экрана 768px +24\n' +
+    '2. Вёрстка соответствует макету. Ширина экрана 380px +24\n' +
+    '3. Ни на одном из разрешений до 320px включительно не появляется горизонтальная полоса прокрутки +15\n' +
+    '4. На ширине экрана 380рх и меньше реализовано адаптивное меню +22\n' +
+    'Итого 85 баллов (все пункты выполнены)'
+)
+
+
+window.addEventListener("load", (event) => {
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+
+    menuBtn.addEventListener('click', function(){
+        menuBtn.classList.toggle('active');
+        menu.classList.toggle('active');
+    })
+});
+
+function closeBurger() {
+    let menuBtn = document.querySelector('.menu-btn');
+    let menu = document.querySelector('.menu');
+
+    menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+}
+
+
